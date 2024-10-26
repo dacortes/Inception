26b46e0ff7fe5161edd71a412348d8643efc5f19
dacortes <87203430+dacortes@users.noreply.github.com>
2024-10-26 16:15:48 +0000

    [IMP] conect nginx to wordpress

---
+++ srcs/requirements/mariadb/conf/mariadb.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+mysqld & 
+sleep 10
+
+
+if [ ! -d /var/lib/mysql/$DATA_BASE_NAME ]; then
+    echo "Creating database..."
+    mysql -e "CREATE DATABASE $DATA_BASE_NAME;"
+    
+    echo "Creating user..."
+    mysql -e "CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+    
+    echo "Granting privileges..."
+    mysql -e "GRANT ALL ON $DATA_BASE_NAME.* TO '$MARIADB_USER'@'%' WITH GRANT OPTION;"
+    
+    echo "Flushing privileges..."
+    mysql -e "FLUSH PRIVILEGES;"
+fi
+
+mysqladmin -u $MARIADB_ROOT_USER --password=$MARIADB_ROOT_PASSWORD shutdown
+sleep 10
+mysqld
+echo "Tailing logs..."
+tail -f
+
